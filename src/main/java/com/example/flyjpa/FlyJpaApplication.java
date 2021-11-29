9acc57e09c956db48ae25b2a7189794a7a755a2a
panqinzhang <panqin.zhang@thoughtworks.com>
2021-11-29 16:12:38 +0800

    build(N/A): 初始化项目

---
+++ src/main/java/com/example/flyjpa/FlyJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.flyjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlyJpaApplication.class, args);
+    }
+
+}
